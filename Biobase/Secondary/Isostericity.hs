1c65ce845f73aeae50ee35acd1fb8eb965955793
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2011-10-05 16:45:18 +0200

    isostericity done

--- Biobase/Secondary/Isostericity.hs
+++ Biobase/Secondary/Isostericity.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 -- | Provides detailed information on isostericity of RNA basepairs. All data
@@ -14,13 +16,44 @@ import Data.ByteString.Char8 (ByteString)
 import Data.FileEmbed (embedFile)
 import Data.List
 import qualified Data.ByteString.Char8 as BS
+import qualified Data.Map as M
 import Text.CSV
 
+import Biobase.Primary
+import Biobase.Secondary
 
+
+
+class IsostericityLookup a where
+  -- | To which classes does a basepair+type belong
+  getClasses :: a -> [String] -- TODO this should return [Class]
+  -- | What basepairs+type are in a particular class
+  inClass :: String -> [a]
+
+instance IsostericityLookup ExtPair where
+  getClasses p@((x,y),t)
+    | Just cs <- M.lookup p defaultIsostericityMap
+    = cs
+    {-
+    | cs <- M.lookup ((y,x),t) defaultIsostericityMap
+    = cs
+    | otherwise = [] -}
+
+defaultIsostericityMap = mkIsostericityMap parsedCSV
+
+-- | mapping of (pair,pairtype) to isostericity class
+
+mkIsostericityMap = M.fromListWith (\x y -> nub $ x++y) . mkIsostericityList
+
+mkIsostericityList gs = concatMap f gs where
+  f g = map (\e -> ((let [x,y] = fst e in (mkNuc x, mkNuc y), threeChar bpt), nub $ snd e)) $ map entry xs where --  (bpt,map entry xs) where
+    bpt = head $ head g
+    xs = tail g
+    entry x = (x!!0, takeWhile ((=='I') . head) $ drop 2 x)
 
 -- | 
 
-parsedCSV = gs where
+parsedCSV = filter (not . null) gs where
   gs = map (filter ((""/=).head)) . groupBy (\x y -> ""/= (head y)) $ csv
   Right csv = parseCSV "isostericity/detailed" $ BS.unpack detailedCSV
 
