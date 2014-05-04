9e374507c8ab5d0cdb11388857169ec853c50d9d
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2014-05-04 21:42:39 +0200

    SubOptDistance now extends all structure lines
    we do not return just the optimal line, but rather all structure lines
    have a third field with the distance

--- SubOptDistance.hs
+++ SubOptDistance.hs
@@ -39,16 +39,31 @@ helpLines = unlines
 main = do
   Options{..} <- cmdArgs options
   (sqn:xs') <- fmap lines $ getContents
+  putStrLn sqn
+  let xs = map (extendStructure structure aq qa) xs'
+  mapM_ (\(s,d) -> printf "%s %6d\n" s d) xs
+  {-
   let xs = map (\(s,e) -> (s,either error id $ dotBracket ["()"] s,read e)) $ map (break isSpace) xs'
   let q = either error id $ dotBracket ["()"] structure
   let (d,x,e) = getMinimalDistance aq qa q xs
   let distance :: Double = e - ((/100) . read $ words sqn !! 1)
   printf "%4d %s %8.2f %8.2f\n" d x e distance
+  -}
 
+extendStructure :: String -> Bool -> Bool -> String -> (String,Int)
+extendStructure strq aq qa s = (s,length $ aqs++qas) where
+  (s',_) = break isSpace s
+  t = either error id $ dotBracket ["()"] s'
+  q = either error id $ dotBracket ["()"] strq
+  aqs = if aq then t \\ q else []
+  qas = if qa then q \\ t else []
+
+{-
 getMinimalDistance :: Bool -> Bool -> [PairIdx] -> [(String,[PairIdx],Double)] -> (Int,String,Double)
 getMinimalDistance aq qa q xs = g $ minimumBy (comparing f) xs where
   g (a,b,c) = ( fst $ f (a,b,c) , a, c)
   f (_,a,e) = (length $ aqs ++ qas , e) where
     aqs = if aq then a \\ q else []
     qas = if qa then q \\ a else []
+-}
 
