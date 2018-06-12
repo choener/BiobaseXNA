5ef9dd44831d9cfa62485d471f60e470674ecd48
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-06-12 15:33:49 +0200

    updated default.nix file

--- default.nix
+++ default.nix
@@ -1,21 +1,29 @@
-with import <nixpkgs> {};
-let
-  packageOverrides = haskellPackages.override {
-    overrides = self: super: {
-      # old doctest
-      pipes-group = haskell.lib.dontCheck super.pipes-group;
-    };
-  };
-  sourceOverrides = packageOverrides.extend (haskell.lib.packageSourceOverrides {
+with (import <nixpkgs> {});
+with haskell.lib;
+
+rec {
+  hsPkgs = haskellPackages.extend (packageSourceOverrides {
     bimaps = ../Lib-bimaps;
     BiobaseENA = ../Lib-BiobaseENA;
     BiobaseTypes = ../Lib-BiobaseTypes;
     BiobaseXNA = ./.;
     DPutils = ../Lib-DPutils;
     ForestStructures = ../Lib-ForestStructures;
+    OrderedBits = ../Lib-OrderedBits;
     PrimitiveArray = ../Lib-PrimitiveArray;
     SciBaseTypes = ../Lib-SciBaseTypes;
   });
-in
-sourceOverrides
-
+  hsShell = with hsPkgs; shellFor {
+    packages = p: [ p.BiobaseXNA ];
+    withHoogle = true;
+    buildInputs = [
+      cabal-install ghc
+      BiobaseENA BiobaseTypes
+      DPutils
+      ForestStructures
+      OrderedBits
+      PrimitiveArray
+      SciBaseTypes
+    ];
+  };
+}
