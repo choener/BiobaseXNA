733c0752924e3bbfcbb0645f92877e46bd06cc38
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-08-20 18:56:36 +0200

    generic default.nix

--- default.nix
+++ default.nix
@@ -1,25 +1,33 @@
-with (import <nixpkgs> {});
+{ pkgs ? <nixpkgs> }:
+with (import pkgs {});
 with haskell.lib;
+with builtins;
 
-rec {
-  hsSrcSet = (lib.foldl' (s: p: s // (import p).hsSrcSet) {} [
-    ../Lib-bimaps
-    ../Lib-BiobaseENA
-    ../Lib-BiobaseTypes
-    ../Lib-ForestStructures
-    ../Lib-PrimitiveArray
-  ]) // {BiobaseXNA = ./.;};
+let
+  # check directories below this one
+  parentContent = readDir ./..;
+  # extract sibling folders that contain a default.nix file
+  parentDirs = filter (d: pathExists (./.. + ("/" + d + "/default.nix"))) (attrNames parentContent);
+  # construct set of names / source directories for override
+  hsSrcSet = listToAttrs (map (d: {name = "${d}"; value = ./.. + ("/" + d);}) parentDirs);
+  # extend the set of packages with source overrides
   hsPkgs = haskellPackages.extend (packageSourceOverrides hsSrcSet);
+  # name of this module
+  this = baseNameOf ./.;
+  traceds = x: y: builtins.trace (builtins.deepSeq x x) y;
+in
+
+{
   hsShell = with hsPkgs; shellFor {
-    packages = p: [ p.BiobaseXNA ];
+    packages = p: let v = p.vector;
+                  in  [ p."${this}" ]; # traceds (p."${this}".buildInputs) [ ];
     withHoogle = true;
     buildInputs = [
-      cabal-install ghc
-      bimaps
-      BiobaseENA
-      BiobaseTypes
-      ForestStructures
-      PrimitiveArray
+      cabal-install
     ];
   };
+  # nix-build -A hsBuild
+  # this shall build and put into ./result
+  # the result is a typical ./bin/; ./lib/ etc.
+  hsBuild = with hsPkgs; callCabal2nix "${this}" ./. {};
 }
