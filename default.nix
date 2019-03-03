07948e58577d0af0c1d49de466c5b3250134ddbf
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-04 00:15:51 +0100

    move to envhs

--- default.nix
+++ default.nix
@@ -1,2 +1,29 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, aeson, attoparsec, base, bimaps, binary, BiobaseENA
+, BiobaseTypes, bytes, bytestring, cereal, cereal-vector, cmdargs
+, containers, csv, data-default, deepseq, file-embed
+, ForestStructures, hashable, lens, mtl, primitive, PrimitiveArray
+, QuickCheck, split, stdenv, tasty, tasty-quickcheck, tasty-th
+, text, tuple, vector, vector-binary-instances, vector-th-unbox
+}:
+mkDerivation {
+  pname = "BiobaseXNA";
+  version = "0.11.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  enableSeparateDataOutput = true;
+  libraryHaskellDepends = [
+    aeson attoparsec base bimaps binary BiobaseENA BiobaseTypes bytes
+    bytestring cereal cereal-vector containers csv data-default deepseq
+    file-embed ForestStructures hashable lens mtl primitive
+    PrimitiveArray QuickCheck split text tuple vector
+    vector-binary-instances vector-th-unbox
+  ];
+  executableHaskellDepends = [ base cmdargs ];
+  testHaskellDepends = [
+    base QuickCheck tasty tasty-quickcheck tasty-th vector
+  ];
+  homepage = "https://github.com/choener/BiobaseXNA";
+  description = "Efficient RNA/DNA/Protein Primary/Secondary Structure";
+  license = stdenv.lib.licenses.gpl3;
+}
