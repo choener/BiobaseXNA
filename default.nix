fe1294a96f9813489f970b896db7da562f87e4be
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2022-05-03 16:06:54 +0200

    updated default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,14 @@
 { mkDerivation, aeson, attoparsec, base, bimaps, binary, BiobaseENA
 , BiobaseTypes, bytes, bytestring, cereal, cereal-vector, cmdargs
-, containers, csv, data-default, deepseq, file-embed
-, ForestStructures, hashable, lens, mtl, primitive, PrimitiveArray
-, QuickCheck, split, stdenv, tasty, tasty-quickcheck, tasty-th
-, text, tuple, vector, vector-binary-instances, vector-th-unbox
+, containers, csv, data-default, deepseq, DPutils, file-embed
+, ForestStructures, hashable, lens, lib, mtl, primitive
+, PrimitiveArray, QuickCheck, split, tasty, tasty-quickcheck
+, tasty-th, text, tuple, vector, vector-binary-instances
+, vector-th-unbox
 }:
 mkDerivation {
   pname = "BiobaseXNA";
-  version = "0.11.0.0";
+  version = "0.11.1.1";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
@@ -15,15 +16,25 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson attoparsec base bimaps binary BiobaseENA BiobaseTypes bytes
     bytestring cereal cereal-vector containers csv data-default deepseq
-    file-embed ForestStructures hashable lens mtl primitive
+    DPutils file-embed ForestStructures hashable lens mtl primitive
     PrimitiveArray QuickCheck split text tuple vector
     vector-binary-instances vector-th-unbox
   ];
-  executableHaskellDepends = [ base cmdargs ];
+  executableHaskellDepends = [
+    aeson attoparsec base bimaps binary BiobaseENA BiobaseTypes bytes
+    bytestring cereal cereal-vector cmdargs containers csv data-default
+    deepseq DPutils file-embed ForestStructures hashable lens mtl
+    primitive PrimitiveArray QuickCheck split text tuple vector
+    vector-binary-instances vector-th-unbox
+  ];
   testHaskellDepends = [
-    base QuickCheck tasty tasty-quickcheck tasty-th vector
+    aeson attoparsec base bimaps binary BiobaseENA BiobaseTypes bytes
+    bytestring cereal cereal-vector containers csv data-default deepseq
+    DPutils file-embed ForestStructures hashable lens mtl primitive
+    PrimitiveArray QuickCheck split tasty tasty-quickcheck tasty-th
+    text tuple vector vector-binary-instances vector-th-unbox
   ];
   homepage = "https://github.com/choener/BiobaseXNA";
   description = "Efficient RNA/DNA/Protein Primary/Secondary Structure";
-  license = stdenv.lib.licenses.gpl3;
+  license = lib.licenses.bsd3;
 }
